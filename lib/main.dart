c57595c49513ca9be674875adb54711d940dbfd6
Edson Moura <edsondinhamais@gmail.com>
2021-04-25 23:40:18 -0300

    levantar ambiente de QA

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:corra_do_corona/login.dart';
 
-class Login extends StatefulWidget {
-  @override
-  _LoginState createState() => _LoginState();
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home:  FirstRoute (),
+  ));
 }
 
-class _LoginState extends State<Login> {
+class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.amberAccent,
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextField(
-                    autofocus: true,
-                    keyboardType: TextInputType.number,
-                    style: TextStyle(color: Colors.blue, fontSize: 30),
-                    decoration: InputDecoration(
-                      labelText:"Telefone do usuário",
-                      labelStyle: TextStyle(color: Colors.black),
-                    )
-                ),
-                Divider(),
-                TextField(
-                    autofocus: true,
-                    obscureText: true,
-                    keyboardType: TextInputType.text,
-                    style: TextStyle(color: Colors.blue, fontSize: 30),
-                    decoration: InputDecoration(
-                      labelText:"Senha do usuário",
-                      labelStyle: TextStyle(color: Colors.black),
-                    )
-                ),
-                Divider(),
-                ButtonTheme(
-                  height: 60.0,
-                  child: RaisedButton(
-                    onPressed: () => {
-                      print("pressionei o botão"),
-                    },
-                    shape: new RoundedRectangleBorder(borderRadius:
-                    new BorderRadius.circular(30.0)),
-                    child: Text(
-                      "Enviar",
-                      style: TextStyle(color: Colors.white, fontSize: 30),
-                    ),
-                    color:Colors.red,
-                  ),
-                ),
-              ],
-            ),
+        appBar: AppBar(
+          title: Text('First Route'),
+        ),
+        body: Center(
+          child: RaisedButton(
+            child: Text('Avançar'),
+            onPressed: (){
+              Navigator.push(
+               context,
+                MaterialPageRoute(builder: (context) => SecondRoute ()),
+              );
+            },
           ),
-        )
+        ),
     );
   }
 }
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+         child: Text('Voltar!')
+        ),
+      )
+    );
+  }
+}
+
+
+
+
