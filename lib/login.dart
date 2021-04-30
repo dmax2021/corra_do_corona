407d04a19c7f011f5d318222ee9936c0b3e85b21
Edson Moura <edsondinhamais@gmail.com>
2021-04-30 00:41:01 -0300

    criando tela de login 2

--- lib/login.dart
+++ lib/login.dart
@@ -13,46 +13,50 @@ class _LoginState extends State<Login> {
         body: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextField(
-                    autofocus: true,
-                    keyboardType: TextInputType.number,
-                    style: TextStyle(color: Colors.blue, fontSize: 30),
-                    decoration: InputDecoration(
-                      labelText:"Email",
-                      labelStyle: TextStyle(color: Colors.black),
-                    )
-                ),
-                Divider(),
-                TextField(
-                    autofocus: true,
-                    obscureText: true,
-                    keyboardType: TextInputType.text,
-                    style: TextStyle(color: Colors.blue, fontSize: 30),
-                    decoration: InputDecoration(
-                      labelText:"Senha",
-                      labelStyle: TextStyle(color: Colors.black),
-                    )
-                ),
-                Divider(),
-                ButtonTheme(
-                  height: 60.0,
-                  child: RaisedButton(
-                    onPressed: () => {
-                      print("pressionei o botão"),
-                    },
-                    shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                    child: Text(
-                      "Enviar",
-                      style: TextStyle(color: Colors.white, fontSize: 30),
-                    ),
-                    color:Colors.red,
+            child: SingleChildScrollView(
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset('assets/images/Logo2.png'),
+                  TextField(
+                      autofocus: true,
+                      keyboardType: TextInputType.number,
+                      style: TextStyle(color: Colors.blue, fontSize: 30),
+                      decoration: InputDecoration(
+                        labelText:"Email",
+                        labelStyle: TextStyle(color: Colors.black),
+                      )
                   ),
-                ),
-              ],
+                  Divider(),
+                  TextField(
+                      autofocus: true,
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      style: TextStyle(color: Colors.blue, fontSize: 30),
+                      decoration: InputDecoration(
+                        labelText:"Senha",
+                        labelStyle: TextStyle(color: Colors.black),
+                      )
+                  ),
+                  Divider(),
+                  ButtonTheme(
+                    height: 60.0,
+                    child: RaisedButton(
+                      onPressed: () => {
+                        print("pressionei o botão"),
+                      },
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                      child: Text(
+                        "Enviar",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                      color:Colors.red,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         )
